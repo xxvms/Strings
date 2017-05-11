95fd1be6fe4a44c069a4a719d2f0e23180ebd59a
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-11 20:17:24 +0100

    _s

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -2,11 +2,22 @@
 //
 
 #include <iostream>
+#include <cstring>
+
 using  namespace std;
 int main()
 {
-	char str[] = "Farewell";
+/*	char str[] = "Farewell";
 	cout << str << endl;
+	cout << str[1] << endl;
 	cin.get();
 	return 0;
+
+	*/
+		char st1[] = "Farewell";
+		char st2[20];
+		cout << strcpy_s(st2, st1) << endl;
+		cin.get();
+		return 0;
+
 }
