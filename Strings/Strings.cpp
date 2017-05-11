45630f61c5a9d5c7798397100786de0dacf07366
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-11 20:27:32 +0100

    new exercise

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -14,10 +14,27 @@ int main()
 	return 0;
 
 	*/
-		char st1[] = "Farewell";
+		/*char st1[] = "Farewell";
 		char st2[20];
 		cout << strcpy_s(st2, st1) << endl;
+		
 		cin.get();
 		return 0;
+*/
+
+	/*	char st1[] = "House";
+		char st2[] = "se";
+		if (strstr(st1, st2) != NULL)
+			cout << "found" << endl;
+		cin.get();
+		return 0;*/
+
+
+		char st1[][5] = { "ed","joe","john" };
+		for (int j = 0; j<3; j++)
+			cout << st1[j] << endl;
+		cin.get();
+		return 0;
+
 
 }
