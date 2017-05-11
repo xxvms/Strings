ff90aa3f605279f28d53d75c4f9a6f68131d9c7e
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-11 20:07:32 +0100

    Add project files.

---
+++ Strings/Strings.cpp
@@ -0,0 +1,12 @@
+// Strings.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using  namespace std;
+int main()
+{
+	char str[] = "Farewell";
+	cout << str << endl;
+	cin.get();
+	return 0;
+}
